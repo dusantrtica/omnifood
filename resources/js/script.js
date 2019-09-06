abc59c22f8c3c90454d4531d513ab610f55b6e56
Dusan <dusan.trtica@hotmail.com>
2019-09-06 16:57:14 +0200

    added javascript and handling scroll events

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,6 +1,15 @@
-$(document).ready(function () {
-    console.log('ready!');
-    $('h1').click(function() {
-        $(this).css('background-color', '#FF0000');
+(function() {
+    const observer = new IntersectionObserver((entries) => {
+        if(entries[0].isIntersecting === true) {
+            console.log('its there');
+            $('nav').addClass('sticky');
+        }
+    }, {threshold: [0.1]});
+
+    observer.observe(document.querySelector('.js--section-features'));
+
+    document.querySelector('.js--scroll-to-plan').addEventListener('click', () => {
+        console.log('clicked!')        
+        document.querySelector('.js--section-plans').scrollIntoView({block: "start", inline: "nearest"});
     })
-});
+})();
